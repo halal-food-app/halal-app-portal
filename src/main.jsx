be0e87e159dc17c8079248260dce67b8e47c2ffe
Ayaansethii <ayaanmsethi@gmail.com>
2026-04-20 20:18:31 -0400

    add api logic to FE

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import AuthProvider from './context/AuthContext.jsx'
+import './index.css'
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
